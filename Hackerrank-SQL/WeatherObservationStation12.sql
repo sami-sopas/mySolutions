e03cca98ba229bb5390ad59c61ce80db484c3f63
Samuel Cuenca <99571985+sami-sopas@users.noreply.github.com>
2025-07-10 16:36:31 -0600

    Create WeatherObservationStation12.sql

---
+++ Hackerrank-SQL/WeatherObservationStation12.sql
@@ -0,0 +1,17 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
+Your result cannot contain duplicates.
+*/
+
+DECLARE @Station AS TABLE (
+    City NVARCHAR(100)
+);
+
+INSERT INTO @Station
+    SELECT CITY FROM STATION;
+
+SELECT DISTINCT
+ City
+FROM @Station
+WHERE LOWER(SUBSTRING(City,1,1)) NOT IN ('a','e','i','o','u')  --First letter
+AND LOWER(SUBSTRING(City,LEN(City), LEN(City))) NOT IN ('a','e','i','o','u') --Last letter
