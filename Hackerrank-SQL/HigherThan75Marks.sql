b8f0c34f79cb9f7a2635125ebdf5efe4b6babad9
Samuel Cuenca <99571985+sami-sopas@users.noreply.github.com>
2025-07-21 16:45:31 -0600

    Create HigherThan75Marks.sql

---
+++ Hackerrank-SQL/HigherThan75Marks.sql
@@ -0,0 +1,17 @@
+-- Query the Name of any student in STUDENTS who scored higher than 75 Marks. 
+-- Order your output by the last three characters of each name.
+-- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+
+;WITH CTE AS (
+    SELECT 
+        *,
+        SUBSTRING([Name],LEN([Name]) - 2, 3) AS LastThree
+    FROM STUDENTS
+    WHERE [Marks] > 75
+)
+
+SELECT 
+    Name
+FROM CTE
+ORDER BY LastThree, Id
