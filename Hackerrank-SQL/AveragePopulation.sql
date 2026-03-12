0532f4a019b0930529d0f9c60c4769227dad8cc2
Samuel Cuenca <99571985+sami-sopas@users.noreply.github.com>
2026-03-12 12:51:00 -0600

    add avg population

---
+++ Hackerrank-SQL/AveragePopulation.sql
@@ -0,0 +1,14 @@
+SET NOCOUNT ON;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT 
+    FLOOR(AVG([Population])) AS [Population]
+FROM City
+
+
+
+go
